59af2086486a5577c510d9d3587cdd3d7cd719fb
SARITHASAKTHIVEL <sarithaasakthi@gmail.com>
2025-04-07 19:39:21 +0530

    correct the domain

--- src/components/ChatPage.js
+++ src/components/ChatPage.js
@@ -19,7 +19,8 @@ const ChatPage = () => {
     setLoading(true);
   
     try {
-      const response = await fetch("http://127.0.0.1:5000/ask", {
+      // const response = await fetch("http://127.0.0.1:5000/ask", {
+        const response = await fetch("https://server-bot-000v.onrender.com/ask", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ question: text }),
